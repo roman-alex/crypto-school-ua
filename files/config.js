2141828164aa2dc2849426109c0238699b74845d
Roman Alekseev <roman.alekseev555@gmail.com>
2022-10-10 20:45:35 +0300

    first commit

---
+++ files/config.js
@@ -0,0 +1,6 @@
+
+window[window["TiktokAnalyticsObject"]].instance("C5IQO3C6J7TSRVQDIMUG").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":true,"auto_email":false,"auto_phone_number":false});
+window[window["TiktokAnalyticsObject"]].instance("C5IQO3C6J7TSRVQDIMUG").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("C5IQO3C6J7TSRVQDIMUG").setPixelInfo({status: 0, name: "Pixel", advertiserID: "7017508142825111553", setupMode: 0, partner: "", is_onsite: false });
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("cryptology.school");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig([]);
+window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie && window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie(true);
